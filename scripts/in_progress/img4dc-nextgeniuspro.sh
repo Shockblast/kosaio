13e28d6837365d3acc9c830b053c3d1b9f2c7dd0
Shockblast <danielmaccomb@gmail.com>
2025-08-01 23:20:34 -0400

    Removed img4dc from the tools as it doesn't work for now.

--- scripts/in_progress/img4dc-nextgeniuspro.sh
+++ scripts/in_progress/img4dc-nextgeniuspro.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
+# DDOES NOT COMPILE / CONTAINS ERRORS
 set -e # Exit immediately if a command exits with a non-zero status.
 
 IMG4DC_DIR="${DREAMCAST_SDK_EXTRAS}/img4dc"
 
 function info() {
 	kosaio-echo "Name: img4dc \n" \
-		"Git: https://github.com/kazade/img4dc.git \n" \
-		"Description: A collection of tools for working with various Dreamcast disc image formats like CDI and MDS."
+		"Git: hhttps://github.com/nextgeniuspro/img4dc.git \n" \
+		"Description: A collection of tools for working with various Dreamcast disc image formats like CDI and MDS. \n" \
+		"Note-1: The original img4dc can be found here https://sourceforge.net/projects/img4dc/"
 }
 
 function dependencies() {
-	kosaio-echo "Nothing to install..."
+	kosaio-echo "Installing img4dc dependencies..."
+	apt-get install -y --no-install-recommends libncurses5-dev
+	kosaio-echo "img4dc dependencies installed..."
 }
 
 function copy-bin() {
-	cp "build/mds4dc/mds4dc" "build/cdi4dc/cdi4dc" "${DREAMCAST_BIN_PATH}"
-	kosaio-echo "img4dc copied."
+	cp "mds4dc/mds4dc" "cdi4dc/cdi4dc" "${DREAMCAST_BIN_PATH}"
+	kosaio-echo "img4dc copied to ${DREAMCAST_BIN_PATH}."
 }
 
 function clone() {
 	kosaio-echo "Cloning img4dc."
-	git clone --depth=1 --single-branch "https://github.com/kazade/img4dc.git" "${IMG4DC_DIR}"
+	git clone --depth=1 --single-branch "https://github.com/nextgeniuspro/img4dc.git" "${IMG4DC_DIR}"
 	kosaio-echo "img4dc has been cloned."
 }
 
 function build() {
 	kosaio-echo "Re/Building img4dc..."
 	cd "${IMG4DC_DIR}"
-	mkdir -p build
-	cd build
-	cmake ..
-	make -j$(nproc)
+	./build.sh
 	kosaio-echo "img4dc has been built."
 }
 
@@ -42,6 +43,7 @@ function update() {
 
 function install() {
 	kosaio-echo "Installing img4dc..."
+	dependencies
 	clone
 	build
 	copy-bin
